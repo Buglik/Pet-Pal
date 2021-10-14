bb809e24b30757f894f8070870b101b7dc593a2f
buglik <kubula243243@gmail.com>
2021-10-15 00:06:42 +0200

    OpenAPI documentation

--- BE/PetPal/PetPal/settings.py
+++ BE/PetPal/PetPal/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users'
+    'drf_spectacular',
+    'users',
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'PetPal API',
+    'DESCRIPTION': 'Pet Sitter Search App documentation',
+    'VERSION': '1.0.0',
+}

e73a9d732c6dfb225593ca1f2c33fffa7c8ffc9f
buglik <kubula243243@gmail.com>
2021-10-14 23:33:51 +0200

    Docker files

--- BE/PetPal/PetPal/settings.py
+++ BE/PetPal/PetPal/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'PetPal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'petpal',
+        'USER': 'root',
+        'PASSWORD':'root',
+        'HOST':'petpal_db',
+        'PORT':'3306'
     }
 }
 

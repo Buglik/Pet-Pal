e130bb604f5017778364ec2254b742c7d20dd223
buglik <kubula243243@gmail.com>
2021-10-24 11:44:26 +0200

    Header navigation

--- FE/src/app/portal/pet-sitters/pet-sitters.component.ts
+++ FE/src/app/portal/pet-sitters/pet-sitters.component.ts
@@ -1,15 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-pet-sitters',
   templateUrl: './pet-sitters.component.html',
   styleUrls: ['./pet-sitters.component.scss']
 })
-export class PetSittersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PetSittersComponent {
 }
--- FE/src/app/portal/portal.module.ts
+++ FE/src/app/portal/portal.module.ts
@@ -2,11 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PortalComponent} from './portal.component';
 import {PortalRoutingModule} from "./portal-routing.module";
+import {HeaderComponent} from './header/header.component';
+import {HeaderLinksComponent} from './header/header-links/header-links.component';
 
 
 @NgModule({
   declarations: [
     PortalComponent,
+    HeaderComponent,
+    HeaderLinksComponent,
   ],
   imports: [
     CommonModule,

2cc9f20a4671894576e4abe396192d809620e17a
buglik <kubula243243@gmail.com>
2021-11-19 18:50:23 +0100

    Sitter update endpoint

--- BE/PetPal/pet_sitters/serializers.py
+++ BE/PetPal/pet_sitters/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Sitter
+from profiles.serializers import MeResponseSerializer
 
 
 class PetSitterRequestSerializer(serializers.ModelSerializer):
@@ -11,3 +12,18 @@ class PetSitterRequestSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return Sitter.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        return instance
+
+
+# class PetSitterResponseSerializer(serializers.ModelSerializer):
+#     profile = MeResponseSerializer()
+#
+#     class Meta:
+#         model = Sitter
+#         fields = ['profile', 'experience', ' pet_experience', 'motivation', 'availability_start_date',
+#                   'availability_end_date']
--- BE/PetPal/pet_sitters/views.py
+++ BE/PetPal/pet_sitters/views.py
@@ -39,3 +39,28 @@ class PetSittersView(views.APIView):
             return Response(status=status.HTTP_201_CREATED)
 
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        request=PetSitterRequestSerializer,
+        responses={200: None},
+    )
+    def put(self, request):
+        token = request.headers['Authorization']
+        token = "".join(token.split()[1])
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+        try:
+            user = decode_access_token(token)
+        except:
+            raise AuthenticationFailed('Unauthenticated!')
+        sitter = Sitter.objects.filter(profile=user.profile).first()
+        if not sitter:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        data = request.data
+
+        serializer = PetSitterRequestSerializer(instance=sitter, data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_200_OK)
